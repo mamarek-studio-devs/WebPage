cebd7f1871576c32b8857c8b014afc5df07650f0
Marek Szkutnik <122742634+mamarek123@users.noreply.github.com>
2026-07-31 03:06:47 +0200

    aj
    asd

--- backend/src/main/java/pl/mamarek/backend/BackendApplication.java
+++ backend/src/main/java/pl/mamarek/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package pl.mamarek;
+package pl.mamarek.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/pl/mamarek/backend/BackendApplicationTests.java
+++ backend/src/test/java/pl/mamarek/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.mamarek;
+package pl.mamarek.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
